5cdf200f3a9445d25239c891fce318b3b3a28b9a
Muhammad Junaidi <emje662@gmail.com>
2024-09-05 19:36:09 +0700

    membuat fitur tambah buku - file baru tambah-buku.php

---
+++ tambah-buku.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Buku</title>
+</head>
+<body>
+    <h1>Fitur tambah Buku</h1>
+</body>
+</html>
